cbfe98af483684a5ba22b1fa43f306bfaf4fcf8c
Chris Knorowski <chris.knorowski@sensiml.com>
2023-04-19 01:09:11 -0700

    update header file

--- sensiml_recognition.h
+++ sensiml_recognition.h
@@ -10,7 +10,8 @@ int get_last_result_model();
 int get_last_result_class();
 int get_last_feature_bank_number();
 void sml_output_results(int model, int classification);
-int sensiml_recognition(signed short *data, int num_sensors);
+int sensiml_recognition(signed short *data, int number_packets, int num_sensors,
+                        int model_index);
 void sensiml_init();
 
 #ifdef __cplusplus
